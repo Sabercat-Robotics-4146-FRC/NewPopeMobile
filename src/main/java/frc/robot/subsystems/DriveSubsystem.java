4146ab51d2d3a0cc9812a292cd27c57b228d68c8
Jake <jakedweiss8@gmail.com>
2022-11-08 15:31:53 -0800

    Test

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -50,6 +50,5 @@ public class DriveSubsystem extends SubsystemBase {
 
   @Override
   public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
   }
 }
